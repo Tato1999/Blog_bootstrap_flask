ab87c9d54d0e89e45b4a2c09c55f3f88107120c3
TatTo <81563835+Tato1999@users.noreply.github.com>
2023-07-14 20:30:31 +0400

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/programing/Desktop/programing/blog_bootstrap/BlogPost/posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////posts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
